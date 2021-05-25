97adfab0b74f822166abe8f6843bf006edcfe375
Zubair Bester <Zubair.bester120390@gmail.com>
2021-05-25 14:24:54 +0200

    Nav and bootstrap installed and login form, Still need to do survey form and then typescript

--- BidvestSurveyTest/src/app/app.module.ts
+++ BidvestSurveyTest/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component'
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
